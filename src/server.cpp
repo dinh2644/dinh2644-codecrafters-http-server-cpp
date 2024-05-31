a5cba4e97438f5292f1ee1772c2616c9c5f85f56
Tu Dinh <tdinh3@cs.binghamton.edu>
2024-05-31 03:20:26 -0400

    pass stage

--- src/server.cpp
+++ src/server.cpp
@@ -15,9 +15,6 @@
 #include <ios>
 #include <iomanip>
 #include <assert.h>
-#include <boost/iostreams/filtering_streambuf.hpp>
-#include <boost/iostreams/copy.hpp>
-#include <boost/iostreams/filter/gzip.hpp>
 
 std::string getRequestBody(std::string &s, std::vector<std::string> &httpVect)
 {
@@ -39,19 +36,33 @@ std::string getRequestBody(std::string &s, std::vector<std::string> &httpVect)
   return result;
 }
 
-static std::string compress_string(const std::string &data)
+std::string compress_string(const std::string &str, int compressionlevel = Z_BEST_COMPRESSION)
 {
-  namespace bio = boost::iostreams;
-  std::istringstream origin(data);
-
-  bio::filtering_istreambuf in;
-  in.push(
-      bio::gzip_compressor(bio::gzip_params(bio::gzip::best_compression)));
-  in.push(origin);
-
-  std::ostringstream compressed;
-  bio::copy(in, compressed);
-  return compressed.str();
+  z_stream zs;
+  memset(&zs, 0, sizeof(zs));
+  if (deflateInit2(&zs, compressionlevel, Z_DEFLATED, 31, 8, Z_DEFAULT_STRATEGY) != Z_OK)
+    throw(std::runtime_error("deflateInit failed while compressing."));
+  zs.next_in = (Bytef *)str.data();
+  zs.avail_in = str.size();
+  int ret;
+  char outbuffer[32768];
+  std::string outstring;
+  do
+  {
+    zs.next_out = reinterpret_cast<Bytef *>(outbuffer);
+    zs.avail_out = sizeof(outbuffer);
+    ret = deflate(&zs, Z_FINISH);
+    if (outstring.size() < zs.total_out)
+    {
+      outstring.append(outbuffer, zs.total_out - outstring.size());
+    }
+  } while (ret == Z_OK);
+  deflateEnd(&zs);
+  if (ret != Z_STREAM_END)
+  {
+    throw(std::runtime_error("Exception during zlib compression: " + std::to_string(ret)));
+  }
+  return outstring;
 }
 
 int main(int argc, char **argv)
