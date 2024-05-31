6be01e7e6526f26926d0a5725e1e42f1f318f907
Tu Dinh <tdinh3@cs.binghamton.edu>
2024-05-30 20:11:14 -0400

    pass stage

--- src/server.cpp
+++ src/server.cpp
@@ -155,13 +155,7 @@ int main(int argc, char **argv)
               std::vector<std::string> httpVect;
               splitHTTPRequest(httpRequest, httpVect);
 
-              std::cout << "httpVect[0]: " << httpVect[0] << "\n";
-              std::cout << "httpVect[1]: " << httpVect[1] << "\n";
-              std::cout << "httpVect[2]: " << httpVect[2] << "\n";
-              std::cout << "httpVect[3]: " << httpVect[3] << "\n";
-              // std::cout << "httpVect[4]: " << httpVect[4] << "\n";
-
-              std::string fileContent = (endPos != std::string::npos) ? httpRequest.substr(startPos, endPos - startPos) : httpRequest.substr(startPos);
+              std::string fileContent = httpRequest;
 
               outputFile << fileContent << std::endl;
               outputFile.close();
