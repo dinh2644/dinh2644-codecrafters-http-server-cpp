d0f3e61ad41901816108461977d251a3885174de
Tu Dinh <tdinh3@cs.binghamton.edu>
2024-05-30 20:30:45 -0400

    pass stage

--- src/server.cpp
+++ src/server.cpp
@@ -160,7 +160,7 @@ int main(int argc, char **argv)
               std::vector<std::string> httpVect;
               splitHTTPRequest(httpRequest, httpVect);
 
-              std::string fileContent = httpRequest;
+              std::string fileContent(httpRequest.rbegin(), httpRequest.rend());
 
               std::cout << "fileContent: " << fileContent << "\n";
 
