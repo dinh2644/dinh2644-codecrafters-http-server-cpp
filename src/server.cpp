84a8aa668bc9c2068a6f129885642f715b2fe142
Tu Dinh <tdinh3@cs.binghamton.edu>
2024-05-30 21:08:29 -0400

    pass stage

--- src/server.cpp
+++ src/server.cpp
@@ -161,7 +161,7 @@ int main(int argc, char **argv)
               {
                 if (!line.empty())
                 {
-                  fileContent += line + "\n";
+                  fileContent += line + "\nWTF";
                 }
               }
 
